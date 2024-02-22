6f38ddac772d01c9d4c9ca12c443490f46494d73
abdel <sghaierabdessalem@gmail.com>
2024-02-22 20:10:56 +0100

    update

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://www.cabinet-bogl.com',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://api.cabinet-bogl.com',
 })
 
 api.interceptors.request.use(
