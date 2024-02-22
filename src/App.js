b0889d48d03c75ee0004be7108664ea1f3cc00c5
abdel <sghaierabdessalem@gmail.com>
2024-02-22 12:26:35 +0100

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { CSpinner, useColorModes } from '@coreui/react'
@@ -34,7 +34,7 @@ const App = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Suspense
         fallback={
           <div className="pt-3 text-center">
@@ -51,7 +51,7 @@ const App = () => {
           <Route path="*" name="Acceuil" element={<DefaultLayout />} />
         </Routes>
       </Suspense>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
