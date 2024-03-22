859118314cb4f9fdcd181a032387e4a68092084b
abdel <sghaierabdessalem@gmail.com>
2024-03-22 22:13:42 +0100

    update version V1.1.0

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -8,7 +8,7 @@ const AppFooter = () => {
         <span className="ms-1">&copy; jurisys-soft 2024 .</span>
       </div>
       <div>
-        <span className="ml-6">V1.0.9</span>
+        <span className="ml-6">V1.1.0</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Cabinet Brudey Ondziel Gnelenga Locko</span>
