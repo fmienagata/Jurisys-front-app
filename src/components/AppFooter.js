cbe864423729fbc9a3f8034ac253a8640e5f10f1
abdel <sghaierabdessalem@gmail.com>
2024-04-30 15:55:39 +0200

    update version

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,7 +5,7 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <span className="ms-1">&copy; jurisys-soft 2024 .</span> V1.04.29
+        <span className="ms-1">&copy; jurisys-soft 2024 .</span> V1.04.30
       </div>
 
       <div className="ms-auto">
